9d9716bda05f72d5d9b74c11f369807eb828060d
Francesco Paciello <kekkopacio@gmail.com>
2021-03-30 17:49:09 +0200

    Initial Commit

---
+++ Breathing Flower/Breathing_FlowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Breathing_FlowerApp.swift
+//  Breathing Flower
+//
+//  Created by Kekko Paciello on 30/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Breathing_FlowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
